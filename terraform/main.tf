da61a7aa4aa0ac07626354e9ca44343ff033e9a0
Soroush Atarod <soroush.atarod@outlook.com>
2022-11-15 19:55:51 +0000

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "soroush.atarod+contino12@contino.io"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "soroush.atarod+contino12@contino.io"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
