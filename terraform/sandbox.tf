9d39ea57e1d0f2a0853b16b1526fad523164f6e7
Soroush Atarod <soroush.atarod@outlook.com>
2023-08-22 21:34:23 +0100

    Update sandbox.tf

--- terraform/sandbox.tf
+++ terraform/sandbox.tf
@@ -69,26 +69,3 @@ module "sandbox_list" {
   account_customizations_name = "sandbox"
 }
 
-
-module "sandbox_one" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "soroush.atarod+sbx-test@contino.io"
-    AccountName               = "sandbox-sbx-test"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "soroush.atarod@contino.io"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-}
